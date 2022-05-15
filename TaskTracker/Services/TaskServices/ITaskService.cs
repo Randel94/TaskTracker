5df73205ae7cbdcc30e948695dd92f93f43fe281
Randel <korotkih.smak@mail.ru>
2022-05-15 20:22:29 +0700

    Создан шаблон TaskService.

---
+++ TaskTracker/Services/TaskServices/ITaskService.cs
@@ -0,0 +1,36 @@
+﻿using TaskTracker.Models;
+
+namespace TaskTracker.Services.TaskServices
+{
+    public interface ITaskService
+    {
+        /// <summary>
+        /// Получение списка задач.
+        /// </summary>
+        /// <returns></returns>
+        Task<List<TaskModel>> GetTaskList();
+        /// <summary>
+        /// Получение описания задачи по идентификатору.
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <returns></returns>
+        Task<TaskModel> GetTask(int taskId);
+        /// <summary>
+        /// Создание задачи.
+        /// </summary>
+        /// <returns></returns>
+        Task<TaskModel> CreateTask(TaskModel task);
+        /// <summary>
+        /// Редактирование задачи.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        Task<TaskModel> UpdateTask(TaskModel task);
+        /// <summary>
+        /// Удаление задачи.
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <returns></returns>
+        Task Delete(int taskId);
+    }
+}
---
+++ TaskTracker/Services/TaskServices/TaskService.cs
@@ -0,0 +1,43 @@
+﻿using TaskTracker.Data;
+using TaskTracker.Models;
+
+namespace TaskTracker.Services.TaskServices
+{
+    public class TaskService : ITaskService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TaskService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<TaskModel>> GetTaskList()
+        {
+            var response = new List<TaskModel>();
+
+            return response;
+        }
+
+        public async Task<TaskModel> GetTask(int taskId)
+        {
+            var response = new TaskModel();
+
+            return response;
+        }
+
+        public async Task<TaskModel> CreateTask(TaskModel task)
+        {
+            return task;
+        }
+
+        public async Task<TaskModel> UpdateTask(TaskModel task)
+        {
+            return task;
+        }
+
+        public async Task Delete(int taskId)
+        {
+        }
+    }
+}
